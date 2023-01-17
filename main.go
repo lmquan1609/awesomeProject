dfa35bfaadfe41efbc5db483d6ca846ba04e8b68
lmquan1609 <luuminhquan155@gmail.com>
2023-01-17 07:56:42 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func main() {
+	dsn := os.Getenv("DBConnectionStr")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := runService(db); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func runService(db *gorm.DB) error {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r.Run()
+})
